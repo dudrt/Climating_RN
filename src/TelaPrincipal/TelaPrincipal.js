5ae72c585473443a02644d01e69a4bf4e99bc27c
dudrt <duduroth9@gmail.com>
2023-12-23 15:01:00 -0300

    Feature:Interface mudada e dias da semana funcionando.

--- src/TelaPrincipal/TelaPrincipal.js
+++ src/TelaPrincipal/TelaPrincipal.js
@@ -10,44 +10,44 @@ export default function TelaPrincipal() {
     const [ViewInfos, setView] = useState()
     const [ViewTop, setViewTop] = useState()
     const [ViewHourInfo, setHourInfo] = useState()
-    
-    global.diasDaSemana = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
+
 
     const FazerRequest = async () => {
         axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${input}?unitGroup=metric&include=hours%2Cdays&lang=pt&key=824BLCVKKZCSN4HLQVJ6HBSPF&conten`)
             .then((response) => {
+                console.log(response)
                 setView(response.data)
                 ArrumarViewTop(response.data, 0)
                 ArrumarViewHour(response.data, 0)
-            });
+            })
+            .catch(function (error) {
+                console.log(error);
+              });
     }
 
     const ArrumarViewTop = async (data, posicao) => {
 
-        var datetime = data.days[posicao].datetime.split("-")
-        const dia = new Date(datetime[2],datetime[1],datetime[0]);
-        const diaDaSemanaNumero = dia.getDay();
-        const diasDaSemana = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
-        const nomeDoDiaDaSemana = diasDaSemana[diaDaSemanaNumero];
-
+        const date = new Date(data.days[posicao].datetime);
+        const options = { weekday: 'long' };
+        const currentDayOfWeek = date.toLocaleString('pt-BR', options);
         const hoje = new Date()
         let agora = hoje.getHours()
         setViewTop(
-            <View>
-                <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "center", margin: "3%" }}>
-                    <View>
-                        <Text style={{ color: "#FFF", fontSize: 22 }}>{data.resolvedAddress}</Text>
-                        <Text style={{ color: "#FFF", fontSize: 77 }}>{data.days[posicao].hours[agora].temp}°</Text>
-                        <Text style={{ color: "#FFF", fontSize: 22 }}>Min. {data.days[posicao].tempmin}° Max. {data.days[posicao].tempmax}°</Text>
-                        <Text style={{ color: "#FFF", fontSize: 26 }}>{nomeDoDiaDaSemana}</Text>
-
+            <View key={"topkey"} >
+                <View key={'view1'} style={{ flexDirection: "row", alignItems: "center", justifyContent: "center", margin: "1%" }}>
+                    <View style={{ width: "52%" }}>
+                        <Text style={{ color: "#FFF", fontSize: 20 }}>{data.resolvedAddress}</Text>
+                        <Text style={{ color: "#FFF", fontSize: 75 }}>{data.days[posicao].hours[agora].temp}°</Text>
+                        <Text style={{ color: "#FFF", fontSize: 20 }}>Min. {data.days[posicao].tempmin}° Max. {data.days[posicao].tempmax}°</Text>
+                        <Text style={{ color: "#FFF", fontSize: 26 }}>{currentDayOfWeek.split(',')[0].charAt(0).toUpperCase()+currentDayOfWeek.split(',')[0].substring(1)}</Text>
                     </View>
                     <Image
+                        key={"imagekey"}
                         source={{ uri: `https://github.com/dudrt/Climating_RN/blob/main/content/${data.days[posicao].icon}.png?raw=true` }}
-                        style={{ width: "44%", height: "85%", marginStart: "3%" }}
+                        style={{ width: "40%", height: "70%" }}
                     />
                 </View>
-                <View style={{ justifyContent: "center", alignItems: "center" }}>
+                <View key={'view2'} style={{ justifyContent: "center", alignItems: "center" }}>
                     <Text style={styles.descricao}>{data.days[posicao].description}</Text>
                 </View>
             </View>
@@ -60,18 +60,18 @@ export default function TelaPrincipal() {
         let arrayView = []
 
         for (var i = agora; i <= 23; i++) {
-
             arrayView.push(
-                <View style={styles.hour_view_unico}>
-                    <Text style={styles.text_hour}>
+                <View key={i} style={styles.hour_view_unico}>
+                    <Text key={`temp${i}`} style={styles.text_hour}>
                         {data.days[posicao].hours[i].temp}°
                     </Text>
                     <Image
+                        key={`imgdias${i}`}
                         source={{ uri: `https://github.com/dudrt/Climating_RN/blob/main/content/${data.days[posicao].hours[i].icon}.png?raw=true` }}
                         style={{ width: 25, height: 25, alignSelf: "center" }}
                     />
-                    <Text style={styles.text_hour}>{data.days[posicao].hours[i].precipprob}%</Text>
-                    <Text style={styles.text_hour}>{data.days[posicao].hours[i].datetime.slice(0, -3)}</Text>
+                    <Text key={`textprecib${i}`} style={styles.text_hour}>{data.days[posicao].hours[i].precipprob}%</Text>
+                    <Text key={`texthour${i}`} style={styles.text_hour}>{data.days[posicao].hours[i].datetime.slice(0, -3)}</Text>
                 </View>
             )
         }
@@ -84,12 +84,19 @@ export default function TelaPrincipal() {
         <View style={styles.container}>
             <View style={styles.top}>
                 <TextInput
+                    placeholderTextColor={"#FFF"}
                     placeholder="Pesquisar cidade"
                     onChangeText={texto => setInput(texto)}
                     value={input}
                     style={styles.input}
                 />
-                <TouchableOpacity style={styles.button_top} onPress={() => FazerRequest()}><Text>Pesq</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.button_top} onPress={() => FazerRequest()}>
+                <Image
+                style={{width:30,height:30}}
+                resizeMode="contain"
+                source={require("../../content/pesquisa-de-lupa.png")}/>
+                
+                </TouchableOpacity>
             </View>
 
             <ScrollView>
@@ -103,24 +110,23 @@ export default function TelaPrincipal() {
                 </View>
                 <View style={styles.resposta}>
                     {ViewInfos != undefined ? (ViewInfos.days.map((componente, index) => (
-                        index+1 < 7 ? (
+                        index < 7 ? (
                             <TouchableOpacity key={index} style={styles.views}>
                                 <Image
-                                    key={index}
+                                    key={index + 20}
                                     source={{ uri: `https://github.com/dudrt/Climating_RN/blob/main/content/${componente.icon}.png?raw=true` }}
                                     style={{ width: 35, height: 35, marginEnd: "2%" }}
                                 />
-                                <Text style={styles.views_text}>{
-                                   global.diasDaSemana[new Date(
-                                    componente.datetime.split("-")[2],
-                                    componente.datetime.split("-")[1],
-                                    componente.datetime.split("-")[0]).getDay()]
+                                <Text style={styles.views_text} key={index + 30}>{
+                                    new Date(componente.datetime).toLocaleString('pt-BR', {weekday : 'long' }).split(',')[0].charAt(0).toUpperCase()+new Date(componente.datetime).toLocaleString('pt-BR', {weekday : 'long' }).split(',')[0].substring(1)
                                     }</Text>
-                                <Text style={styles.views_text}>Temp:{componente.tempmin}° - </Text>
-                                <Text style={styles.views_text}>{componente.tempmax}°</Text>
+                                <View style={styles.view_text_view}>
+                                    <Text key={index + 40} style={styles.views_text}>Temp:{componente.tempmin}° - </Text>
+                                    <Text key={index + 50} style={styles.views_text}>{componente.tempmax}°</Text>
+                                </View>
                             </TouchableOpacity>
                         ) : (<></>)
-                    ))) : (<><Text>Não existe</Text></>)}
+                    ))) : (<View style={styles.view_pesquisar}><Text style={styles.text_pesquisar}>Pesquise a Cidade Desejada!</Text></View>)}
                 </View>
             </ScrollView>
         </View>
@@ -136,7 +142,7 @@ const styles = StyleSheet.create({
         flex: 1
     },
     top: {
-        marginTop: "10%",
+        marginTop: "15%",
         width: "100%",
         minHeight: 40,
         flexDirection: "row",
@@ -154,12 +160,12 @@ const styles = StyleSheet.create({
         fontSize: 18
     },
     button_top: {
-        backgroundColor: "#E52828",
-        width: "15%",
+        // backgroundColor: "#E52828",
+        // width: "15%",
         marginStart: "4%",
-        borderRadius: 30,
-        justifyContent: "center",
-        alignItems: "center"
+        // borderRadius: 30,
+        // justifyContent: "center",
+        // alignItems: "center"
     },
     resposta: {
         width: "100%",
@@ -202,10 +208,23 @@ const styles = StyleSheet.create({
         margin: 5,
         justifyContent: "center",
         alignContent: "center",
-        // margin:5,
     },
     text_hour: {
         alignSelf: "center",
         color: "#FFF"
+    },
+    view_text_view:{
+        marginStart:"3%",
+        width:"60%",
+        flexDirection:"row"
+    },
+    view_pesquisar:{
+        width:'100%',
+        justifyContent:"center",
+        alignItems:"center",
+    },
+    text_pesquisar:{
+        fontSize:25,
+        color:"#FFF"
     }
 })
